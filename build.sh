0e17b461dc50093a38b0d75c21dcf383ae8be1f1
Kiblik <kiblik@gjh.sk>
2017-02-21 02:50:48 +0100

    este mala oprava

--- build.sh
+++ build.sh
@@ -1,23 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+set -x
 git clone git://git.buildroot.net/buildroot
 cp buildroot.config buildroot/.config
-cd buildroot
+pushd buildroot
 
 make
 
-echo "sd* root:root 444 @myplayer $MDEV" >> output/target/etc/mdev.conf
-
-cat << EOF > output/target/usr/sbin/myplayer
-mkdir -p /mnt/$1
-mount /dev/$1 /mnt/$1
-find /mnt/$1/ -name '*.mp3' -print0 | xargs -0 mpg123
-umount /dev/$1              
-EOF
+cp ../mdev.conf output/target/etc/mdev.conf
+cp ../myplayer output/target/usr/sbin/myplayer
 chmod +x output/target/usr/sbin/myplayer
 
 make
 
-cd ..
+popd
 mkdir -p boot
 cp buildroot/output/images/rpi-firmware/* buildroot/output/images/bcm2709-rpi-2-b.dtb buildroot/output/images/zImage boot
 cp buildroot/output/images/rootfs.ext4 rootfs.img
